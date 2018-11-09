5cd05b189fb34e3a98919a4fa3e17bc758d8f97c
vitorino87 <brother_87_22@hotmail.com>
2018-11-09 16:12:02 -0200

    possui o EJBRemote_Enterprise-ejb

---
+++ EJBRemote_Enterprise-ejb/src/java/ejb/MySession.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author tiago.lucas
+ */
+@Stateless
+public class MySession implements MySessionRemote {
+
+    public String getResult() {
+        return "Hello my Session Bean";
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
